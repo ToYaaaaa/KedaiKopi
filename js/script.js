30b2e3cec342e5ebf645dd9e7fcaf250ac9852a3
ToYaaaaa <140059855+ToYaaaaa@users.noreply.github.com>
2023-08-29 16:32:12 +0700

    perbaikan responsive
    perbaikan pada bagian responise

--- js/script.js
+++ js/script.js
@@ -19,9 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Daftar teks yang ingin Anda ganti secara berurutan
   const newTexts = [
-    "Selamat datang di KedaiKopi! Kami adalah tempat yang sepenuhnya didedikasikan untuk menyajikan pengalaman kopi terbaik bagi pecinta kopi sejati.Dengan lebih dari satu dekade pengalaman dalam dunia kopi,kami dengan bangga mempersembahkan rasa dan aroma kopi terbaik dari seluruh penjuru dunia kepada pelanggan setia kami.",
-    "Di KedaiKopi, komitmen kami adalah memberikan kualitas kopi yang tak tertandingi. Kami percaya bahwa setiap cangkir kopi harus menjadi pengalaman yang memuaskan, mulai dari biji kopi pilihan hingga proses penyajian yang cermat. Kami bekerja erat dengan para petani kopi untuk memastikan pasokan biji kopi berkualitas tinggi.",
-    "Filosofi kami di KedaiKopi adalah menciptakan tempat yang lebih dari sekadar kedai kopi biasa. Kami ingin menciptakan ruang yang mengundang orang untuk datang bersama, berbagi cerita, dan menikmati momen-momen istimewa dalam hidup mereka. Kami percaya bahwa kopi memiliki kekuatan untuk menyatukan orang dari berbagai latar belakang.",
+    "Selamat datang di KedaiKopi! Kami adalah tempat yang sepenuhnya didedikasikan untuk menyajikan pengalaman kopi terbaik bagi pecinta kopi sejati.Dengan lebih dari satu dekade pengalaman dalam dunia kopi.",
+    "Di KedaiKopi, komitmen kami adalah memberikan kualitas kopi yang tak tertandingi. Kami percaya bahwa setiap cangkir kopi harus menjadi pengalaman yang memuaskan.",
+    "Filosofi kami di KedaiKopi adalah menciptakan tempat yang lebih dari sekadar kedai kopi biasa. Kami ingin menciptakan ruang yang mengundang orang untuk datang bersama, berbagi cerita.",
   ];
 
   let currentIndex = 0;
